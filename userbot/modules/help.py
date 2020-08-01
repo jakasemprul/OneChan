4b6f5ef72887f7715c93a7341914571f91cad916
Ilham_Maulana <61121214+Ilham94@users.noreply.github.com>
2020-08-01 14:45:11 +0700

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -9,7 +9,7 @@ from userbot import CMD_HELP
 from userbot.events import register
 
 
-@register(outgoing=True, pattern="^\.help(?: |$)(.*)")
+@register(outgoing=True, pattern="^.help(?: |$)(.*)")
 async def help(event):
     """ For .help command,"""
     args = event.pattern_match.group(1).lower()
@@ -17,23 +17,12 @@ async def help(event):
         if args in CMD_HELP:
             await event.edit(str(CMD_HELP[args]))
         else:
-            await event.edit("`Hadehhh ente -_- kelamaan jomlo ya gini, masukin module aja salah wkwk`")
+            await event.edit("Please specify a valid module name.")
     else:
-        head = "**Help for** [DaengServer](https://github.com/daenghunter/ProjectBish-1)"
-        head2 = "Silahkan menggunakan module yang disediakan dibawah :)"
-        head3 = "Contoh: .help <module name>"
-        head4 = "List Perintah Yang Aktif Saat Ini: "
-        string = ""
-        sep1 = "••••••••••••••••••••••••••••••••••••••••••••••"
-        sep2 = "••••••••••••••••••••••••••••••••••••••••••••••"
+        await event.edit("Please specify which module do you want help for !!"
+                         "\nUsage: .help <module name>")
+        string = "-  "
         for i in CMD_HELP:
             string += "`" + str(i)
-            string += "`  •  "
-        await event.edit(f"{head}\
-              \n{sep2}\
-              \n{head2}\
-              \n{head3}\
-              \n{sep2}\
-              \n{head4}\
-              \n\n{string}\
-              \n{sep1}")
+            string += "`  -  "
+        await event.reply(string)
