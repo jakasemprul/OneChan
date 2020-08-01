ca2e05817805eacf21d8bf5b539aa0c0683031a2
Ilham_Maulana <61121214+Ilham94@users.noreply.github.com>
2020-08-01 18:01:48 +0700

    Add files via upload

--- userbot/modules/sangmata.py
+++ userbot/modules/sangmata.py
@@ -1,45 +1,51 @@
-#Port to userbot by @KeselekPermen69
+from asyncio.exceptions import TimeoutError
 
-import datetime
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
-from telethon.tl.functions.account import UpdateNotifySettingsRequest
-from userbot.events import register
-from userbot import bot, CMD_HELP
 
-@register(outgoing=True, pattern="^\.sg(?: |$)(.*)")
+from userbot import CMD_HELP, bot
+from userbot.events import register
+
+
+@register(outgoing=True, pattern=r"^\.sg(?: |$)(.*)")
 async def _(event):
     if event.fwd_from:
-        return 
+        return
     if not event.reply_to_msg_id:
-       await event.edit("`Reply to any user message.`")
-       return
-    reply_message = await event.get_reply_message() 
+        await event.edit("`Reply to any user message.`")
+        return
+    reply_message = await event.get_reply_message()
     if not reply_message.text:
-       await event.edit("```reply to text message```")
-       return
+        await event.edit("```reply to text message```")
+        return
     chat = "@SangMataInfo_bot"
-    sender = reply_message.sender
-    if reply_message.sender.bot:
-       await event.edit("`Reply to actual users message.`")
-       return
-    await event.edit("`Sedang di proses juragan`")
-    async with bot.conversation(chat) as conv:
-          try:     
-              response = conv.wait_event(events.NewMessage(incoming=True,from_users=461843263))
-              await bot.forward_messages(chat, reply_message)
-              response = await response 
-          except YouBlockedUserError: 
-              await event.reply("`Unblock @sangmatainfo_bot dulu bos, biar botnya bisa jalan -_-`")
-              return
-          if response.text.startswith("Forward"):
-             await event.edit("`Njerrr gak bisa di gangbang`")
-          else: 
-             await event.edit(f"{response.message.message}")
+    await event.edit("`Processing`")
+    try:
+        async with bot.conversation(chat) as conv:
+            try:
+                response = conv.wait_event(
+                    events.NewMessage(incoming=True, from_users=461843263)
+                )
+                await bot.forward_messages(chat, reply_message)
+                response = await response
+                await bot.send_read_acknowledge(conv.chat_id)
+            except YouBlockedUserError:
+                await event.reply("`Please unblock @sangmatainfo_bot and try again`")
+                return
+            if response.text.startswith("Forward"):
+                await event.edit(
+                    "`Can you kindly disable your forward privacy settings for good?`"
+                )
+            else:
+                await event.edit(f"{response.message.message}")
+                await bot.send_read_acknowledge(conv.chat_id)
+    except TimeoutError:
+        return await event.edit("`Error: `@SangMataInfo_bot` is not responding!.`")
 
 
-CMD_HELP.update({
-        "sangmata": 
-        ".sg \
+CMD_HELP.update(
+    {
+        "sangmata": ">`.sg` \
           \nUsage: View user history.\n"
-    })
+    }
+)
