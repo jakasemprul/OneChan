1f907dcc55114a594a41a8b10e3f14252e813ebd
Ilham_Maulana <61121214+Ilham94@users.noreply.github.com>
2020-08-01 17:57:17 +0700

    Add files via upload

--- userbot/utils/__init__.py
+++ userbot/utils/__init__.py
@@ -17,4 +17,11 @@
 from .chrome import chrome, options
 from .google_images_download import googleimagesdownload
 from .progress import progress
-from .tools import human_to_bytes, humanbytes, md5, time_formatter
+
+from .tools import (
+    humanbytes,
+    admin_cmd,
+    time_formatter,
+    human_to_bytes,
+    md5
+)
