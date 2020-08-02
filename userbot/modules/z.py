78a365519117c775d3b235cee9e2023c77830e45
Ilham_Maulana <61121214+Ilham94@users.noreply.github.com>
2020-08-02 20:57:56 +0700

    Update z.py

--- userbot/modules/z.py
+++ userbot/modules/z.py
@@ -6,9 +6,9 @@ from userbot import CMD_HELP, ZALG_LIST
 from userbot.events import register
 
 @register(outgoing=True, pattern='^.p(?: |$)(.*)')
-async def typewriter(wannasee):
-	await wannasee.edit("[DiSini](https://telegra.ph/il-ham-08-02-2) Generate Session String")
+async def typewriter(typew):
+	await typew.edit("[DiSini](https://telegra.ph/il-ham-08-02-2) Generate Session String")
         sleep(1)
-	await wannasee.edit("`Ucap Salam Goblok...`")
+	await typew.edit("`Ucap Salam Goblok...`")
 	sleep(1)
-	await wannasee.edit("`Assalamualaikum`")
+	await typew.edit("`Assalamualaikum`")
