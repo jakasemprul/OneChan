37bfed7b35be0cabc63d0b78d2bb928cdc2d47af
Ilham_Maulana <61121214+Ilham94@users.noreply.github.com>
2020-08-02 13:04:37 +0700

    Update oi.py

--- userbot/modules/oi.py
+++ userbot/modules/oi.py
@@ -7,7 +7,5 @@ from userbot.events import register
 
 @register(outgoing=True, pattern='^.oi(?: |$)(.*)')
 async def typewriter(wannasee):
-	message = wannasee.pattern_match.group(1)
-	sleep(1)
 	await wannasee.edit("`Click [here](https://github.com/Ilham94/OneChan)`")
 	
