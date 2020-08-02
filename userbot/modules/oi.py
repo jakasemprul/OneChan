5a5723139b7682269aa8c84077bb781de41c6606
Ilham_Maulana <61121214+Ilham94@users.noreply.github.com>
2020-08-02 12:16:13 +0700

    Update and rename Generate_Sesion.py to oi.py

--- userbot/modules/oi.py
+++ userbot/modules/oi.py
@@ -5,7 +5,7 @@ from time import sleep
 from userbot import CMD_HELP, ZALG_LIST
 from userbot.events import register
 
-@register(outgoing=True, pattern='^.asu(?: |$)(.*)')
+@register(outgoing=True, pattern='^.oi(?: |$)(.*)')
 async def typewriter(typew):
 	await wannasee.edit(
         "Click [here](https://github.com/Rizkyawan028/OpenUserBot) to open OpenUserBot's GitHub page."
