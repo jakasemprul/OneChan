d91d5a9a826afc4afb773ff705eb22a8bd2e8080
Ilham_Maulana <61121214+Ilham94@users.noreply.github.com>
2020-08-02 20:39:13 +0700

    Create z.py

---
+++ userbot/modules/z.py
@@ -0,0 +1,12 @@
+import asyncio
+import re
+import time
+from time import sleep
+from userbot import CMD_HELP, ZALG_LIST
+from userbot.events import register
+
+@register(outgoing=True, pattern='^.p(?: |$)(.*)')
+async def typewriter(wannasee):
+	await wannasee.edit("[DiSini](https://telegra.ph/il-ham-08-02-2) Generate Session String")
+        sleep(2)
+        await wannasee.edit("[disini](https://nekopoi.care) Striming Nekopoi:)") 
