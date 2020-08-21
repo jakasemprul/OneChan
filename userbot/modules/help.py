579191f45fc4c148c6a520163e2659a84fb02c1f
Ilham_Maulana <61121214+Ilham94@users.noreply.github.com>
2020-08-22 02:49:31 +0700

    I

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -9,9 +9,9 @@ from userbot import CMD_HELP
 from userbot.events import register
 
 
-@register(outgoing=True, pattern="^!help(?: |$)(.*)")
+@register(outgoing=True, pattern="^.help(?: |$)(.*)")
 async def help(event):
-    """ For !help command,"""
+    """ For .help command,"""
     args = event.pattern_match.group(1).lower()
     if args:
         if args in CMD_HELP:
